67a419e30f1a23c6b8f6b2690f107a0d2ed4ba93
CodeWizard-1 <>
2023-10-17 08:28:29 +0000

    Add class Student

--- evens.py
+++ evens.py
@@ -1,4 +1,4 @@
-def even_number_of_evens(number):
+def even_number_of_evens(numbers):
     """
     Should Raise a TypeError if a list in not passed into the function
     error message: "A list was not passed into the function"
@@ -7,8 +7,10 @@ def even_number_of_evens(number):
     if the numner of even numbers is even - return True
     """
 
-    if isinstance(number, list):
-        return True
+    if isinstance(numbers, list):
+        evens = sum([1 for n in numbers if n % 2 == 0])
+
+        return True if evens and evens % 2 == 0 else False
     else:
         raise TypeError("A list was not passed into the function")
 
